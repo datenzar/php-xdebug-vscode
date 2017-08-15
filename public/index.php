25b8cc955f8bb4667d21202df5c388f37908476e
Sneaker15 <github@outgoer.de>
2017-08-15 15:08:47 +0200

    initial commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<a href="get-info.php">Environment Information</a>
+<br>
+<a href="hello_world.php">Hello World</a>
+<br>
+<a href="output.php">Output</a>
+<br>
+<a href="function.php">Function</a>
+<br>
+<a href="variables.php">Variables</a>
+<br>
+<a href="error.php">Error</a>
